b9e44be22098111395fa3e9efd84a2e2b538447e
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-04-29 17:04:59 -0500

    Added player movement, added a start timer

---
+++ Assets/Scripts/CountdownBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownBehavior : MonoBehaviour
+{
+    public float timeToStart = 5;
+    public TextMesh countdown;
+    public MovementBehavior playerMovement;
+    public GravityBehavior gravity;
+
+    void Start()
+    {
+        countdown.alignment = TextAlignment.Center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToStart -= Time.deltaTime;
+        countdown.text = ((int)timeToStart).ToString();
+        if(timeToStart < 1f)
+        {
+            gameObject.SetActive(false);
+            playerMovement.enabled = true;
+            gravity.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/MovementBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovementBehavior : MonoBehaviour
+{
+    public float levelTowMovementSpeed = 5;
+    public float levelOneMovementSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Running")
+        {
+            Vector3 movementDirection = new Vector3(0, 0, 0);
+
+            movementDirection += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movementDirection.Normalize();
+
+            movementDirection *= levelTowMovementSpeed;
+
+            transform.Translate(movementDirection * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 movement = new Vector3(levelOneMovementSpeed, 0, 0);
+
+            transform.Translate(movement * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Obstacle")
+        {
+            enabled = false;
+        }
+    }
+}
