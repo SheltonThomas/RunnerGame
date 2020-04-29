e745f93827ccc1fa2bed1dc15566542ce855ee35
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-04-29 15:18:01 -0500

    Initial commit

---
+++ Assets/Scripts/CameraFollowBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBehavior : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 distanceFromTarget;
+
+    private float yPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceFromTarget = transform.position - target.position;
+        yPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = distanceFromTarget + target.position;
+        cameraPosition.y = yPosition;
+        transform.position = cameraPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/GravityBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBehavior : MonoBehaviour
+{
+    public float gravity = 50;
+
+    private bool canSwapGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, -gravity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempGravity = Physics.gravity;
+        if(Input.GetButtonDown("Jump") && canSwapGravity)
+        {
+            Physics.gravity = -tempGravity;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Wall")
+        {
+            canSwapGravity = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Wall")
+        {
+            canSwapGravity = false;
+        }
+    }
+}
